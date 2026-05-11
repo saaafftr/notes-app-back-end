c2f09c79b1d0d24dcb28c80b6a75106e5425c707
saaafftr <muihui3@gmail.com>
2026-05-11 12:33:42 +0700

    update kodingan ke fundamental originnya *

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import routes from '../routes/index.js';
 import ErrorHandler from '../middlewares/error.js';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use(routes);
 app.use(ErrorHandler);
