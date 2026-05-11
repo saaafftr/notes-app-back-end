4087f0e4b50a28e182be30529ef81a134fb63bbf
saaafftr <muihui3@gmail.com>
2026-05-11 11:55:01 +0700

    Update kodingan ke versi fundamental

---
+++ src/exceptions/index.js
@@ -0,0 +1,4 @@
+import ClientError from './client-error.js';
+import InvariantError from './invariant-error.js';
+import NotFoundError from './not-found-error.js';
+export { ClientError, InvariantError, NotFoundError };
---
+++ src/services/notes/routes/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import {
+  createNote,
+  deleteNoteById,
+  editNoteById,
+  getNotes,
+  getNoteById,
+} from '../controller/note-controller.js';
+import { validate, validateQuery } from '../../../middlewares/validate.js';
+import {
+  notePayloadSchema,
+  noteQuerySchema,
+} from '../../../services/notes/validator/schema.js';
+
+const router = express.Router();
+router.post('/notes', validate(notePayloadSchema), createNote);
+router.get('/notes', validateQuery(noteQuerySchema), getNotes);
+router.get('/notes/:id', getNoteById);
+router.put('/notes/:id', validate(notePayloadSchema), editNoteById);
+router.delete('/notes/:id', deleteNoteById);
+
+export default router;
